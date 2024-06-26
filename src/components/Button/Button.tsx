3392d1bcd075eea2cda4526f52671640068aa2f4
iti-rina <itigecheva.irina@gmail.com>
2024-06-26 22:37:23 +0700

    feat: init button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,14 +2,12 @@ import { FC } from "react";
 
 interface ButtonProps {
   label: string;
-  counter: number;
 }
 
-export const Button: FC<ButtonProps> = ({label, counter}) => {
+export const Button: FC<ButtonProps> = ({label}) => {
   return (
     <button>
       {label}
-      {counter}
     </button>
   );
 }
